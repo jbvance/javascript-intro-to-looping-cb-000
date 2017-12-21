6214aa9a9040689696ddf5b635329d4ff56d408f
Jason Vance <jbvance@gmail.com>
2017-12-21 10:30:56 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,5 +1,7 @@
-var forLoop = (array) => {
+var forLoop = array => {
   for (let i = 0; i < 25; i++) {
-    i === 1 ? array.push("I am 1 strange loop.") : array.push(`I ${i} strange loops.`)
+    i === 1
+      ? array.push('I am 1 strange loop.')
+      : array.push(`I ${i} strange loops.`);
   }
-}
+};
